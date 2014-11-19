1639b0dc33de43aa7b59c6a016af9336e73bdb29
Tann <bonk@ThreeChoiceGames.com>
2014-11-19 18:47:00 +0000

    hm

--- core/src/util/update/InputHandler.java
+++ core/src/util/update/InputHandler.java
@@ -27,7 +27,7 @@ public class InputHandler implements InputProcessor {
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		boolean esc=Screen.isActiveType(EscapeMenu.class);
-		Mouser.updateClicked(button==0);
+		if(Mouser.updateClicked(button==0))return true;
 		if(!esc)Main.touchDown(new Pair((float)screenX/(float)Gdx.graphics.getWidth()*Main.width, ((float)screenY/(float)Gdx.graphics.getHeight()*Main.height)),button==0);
 		
 		return false;
